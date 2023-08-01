0b4979fe8d80ae1ffb4be8d48987bf3d2856cd01
ryan-bennett <ryan.bennett.au@gmail.com>
2023-08-02 08:45:13 +1000

    Removed docusarus imagery

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,7 +1,5 @@
 import React, {FC, PropsWithChildren} from "react";
 
-import {ChakraProvider} from '@chakra-ui/react';
-
 
 const Root: FC<PropsWithChildren> = ({children}) => {
     return (<>{children}</>);
